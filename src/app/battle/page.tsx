493037c0d8ff99f5ee8ea6f45f4e8e86816a361c
Jordi Martinez <jordiupc87@gmail.com>
2024-02-24 22:53:19 +0100

    Testing github deploy

---
+++ src/app/battle/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import EventCard from "../components/EventCard";
+
+type Props = {};
+
+export default function Battle() {
+  return (
+    <div>
+      <EventCard></EventCard>
+    </div>
+  );
+}
