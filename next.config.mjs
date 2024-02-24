6d3339e4c0779b31507b8ac4bec4e633d7cf6793
Jordi Martinez <jordiupc87@gmail.com>
2024-02-24 23:37:21 +0100

    nextjs cfg updated for github publish

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // git publish options
-    output: "export",  // <=== enables static exports
-  reactStrictMode: true,
-  // git publish options
+   
 };
 
 export default nextConfig;
--- src/app/battle/page.tsx
+++ src/app/battle/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import EventCard from "../components/EventCard";
+import PlusIcon from "../icons/plus";
 
 type Props = {};
 
@@ -7,6 +8,7 @@ export default function Battle() {
   return (
     <div>
       <EventCard></EventCard>
+      <PlusIcon></PlusIcon>
     </div>
   );
 }
